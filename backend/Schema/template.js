f2d1acfe407690cdc87abdde082c277e3d5d5d56
jemmyasjd <jemmy33.jd@gmail.com>
2024-06-29 19:01:50 +0530

    delete backend

--- backend/Schema/template.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const TemplateSchema = new mongoose.Schema({
-  templateName : { type: String,required: true },
-});
-
-const TemplateModel = mongoose.model("Template", TemplateSchema);
-
-module.exports = TemplateModel;
